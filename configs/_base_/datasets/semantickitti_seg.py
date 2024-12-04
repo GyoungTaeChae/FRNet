5823679a9bde5b44ba2182168d71e96956597fc8
GyoungTaeChae <foolips@naver.com>
2024-12-04 21:30:35 +0900

    fd

--- configs/_base_/datasets/semantickitti_seg.py
+++ configs/_base_/datasets/semantickitti_seg.py
@@ -5,45 +5,11 @@
 dataset_type = 'SemanticKittiDataset'
 data_root = 'data/semantickitti/'
 class_names = [
-    'car', 'bicycle', 'motorcycle', 'truck', 'bus', 'person', 'bicyclist',
-    'motorcyclist', 'road', 'parking', 'sidewalk', 'other-ground', 'building',
-    'fence', 'vegetation', 'trunck', 'terrian', 'pole', 'traffic-sign'
+ 'building'
 ]
 labels_map = {
-    0: 19,  # "unlabeled"
-    1: 19,  # "outlier" mapped to "unlabeled" --------------mapped
-    10: 0,  # "car"
-    11: 1,  # "bicycle"
-    13: 4,  # "bus" mapped to "other-vehicle" --------------mapped
-    15: 2,  # "motorcycle"
-    16: 4,  # "on-rails" mapped to "other-vehicle" ---------mapped
-    18: 3,  # "truck"
-    20: 4,  # "other-vehicle"
-    30: 5,  # "person"
-    31: 6,  # "bicyclist"
-    32: 7,  # "motorcyclist"
-    40: 8,  # "road"
-    44: 9,  # "parking"
-    48: 10,  # "sidewalk"
-    49: 11,  # "other-ground"
-    50: 12,  # "building"
-    51: 13,  # "fence"
-    52: 19,  # "other-structure" mapped to "unlabeled" ------mapped
-    60: 8,  # "lane-marking" to "road" ---------------------mapped
-    70: 14,  # "vegetation"
-    71: 15,  # "trunk"
-    72: 16,  # "terrain"
-    80: 17,  # "pole"
-    81: 18,  # "traffic-sign"
-    99: 19,  # "other-object" to "unlabeled" ----------------mapped
-    252: 0,  # "moving-car" to "car" ------------------------mapped
-    253: 6,  # "moving-bicyclist" to "bicyclist" ------------mapped
-    254: 5,  # "moving-person" to "person" ------------------mapped
-    255: 7,  # "moving-motorcyclist" to "motorcyclist" ------mapped
-    256: 4,  # "moving-on-rails" mapped to "other-vehic------mapped
-    257: 4,  # "moving-bus" mapped to "other-vehicle" -------mapped
-    258: 3,  # "moving-truck" to "truck" --------------------mapped
-    259: 4  # "moving-other"-vehicle to "other-vehicle"-----mapped
+    0: 0,  # "unlabeled"
+    1: 1,  # "outlier" mapped to "unlabeled" --------------mapped
 }
 metainfo = dict(
     classes=class_names, seg_label_mapping=labels_map, max_label=259)
